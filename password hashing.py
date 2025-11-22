17efe819259eb7492c629336b68abbb0787677fb
rejoy <rejoyjohny@gmail.com>
2025-11-22 23:19:23 +0530

    removed some files and folder

--- password hashing.py
+++
@@ -1,8 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-hashed_password = pwd_context.hash("teacher123")
-
-print(hashed_password)
-# Copy the output. It will look something like:
-# $2b$12$EixZaYVK1fsnwPKKFFBgce................
